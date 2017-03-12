14d255ccd1393bb6cee2320040dea73924fa2f6e
Andrew Williams-Gilchrist <Andrew Williams-Gilchrist>
2017-03-12 14:26:51 -0600

    Actually finished. Added hyperlinks

--- scratchpad.js
+++ scratchpad.js
@@ -1,5 +1,13 @@
 document.title = "Cast off the shackles of HTML";
-document.body.style.width = "20000px";
+document.body.style.width = "25000px";
+
+//Creates a hyperlink to a given site
+var linkSite = function(url, linkText) {
+	var aObj = document.createElement("a");
+	aObj.href = url;
+	aObj.textContent = linkText;
+	return aObj;
+}
 
 //A simple function to create text content for a node
 var nContent = function(n, nType, nValue) {
@@ -122,7 +130,6 @@ var trib = function tribonacci(n) {
 	return{"value" : value, "div" : div};
 }
 
-//TODO: FIx window width
 var style = document.createElement("style");
 style.textContent = ".tree {display: inline-block; width: 15000 px; border-color: rgb(255,0,0); background: rgb(180,60,60); box-shadow: 1px 1px 2px rgba(200,0,0,0.4); text-align: center; padding: 3px 10px;} "
 + ".node {background-color: rgba(0,0,255,0.1);} .node-left {float: left; display: inline-block; left-margin: 6px} " 
@@ -149,3 +156,12 @@ document.querySelector("body").appendChild(tribRoot);
 display(11, fib, document.querySelector(".fib"));
 display(11, pell, document.querySelector(".pell"));
 display(11, trib, document.querySelector(".trib"));
+
+var linkDiv = document.createElement("div");
+var linkSpace = document.createElement("p");
+linkSpace.textContent = "\n\n";
+linkDiv.appendChild(linkSpace);
+linkDiv.appendChild(linkSite("https://oeis.org/A000045", "fibonacci "));
+linkDiv.appendChild(linkSite("https://oeis.org/A000129", "pell "));
+linkDiv.appendChild(linkSite("https://oeis.org/A000073", "tribonacci "));
+document.querySelector("body").appendChild(linkDiv);
